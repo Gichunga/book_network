0db2cc903c148b7f2ef6a7cc4d36946f7c9237f0
gichunga <gichungasteve6@gmail.com>
2024-09-26 16:55:56 +0300

    book entity, feedback entity, booktransactionshisory entity and their relationships

---
+++ src/main/java/com/gichungasoftwares/book_network/common/BaseEntity.java
@@ -0,0 +1,2 @@
+package com.gichungasoftwares.book_network.common;public class BaseEntity {
+}
---
+++ src/main/java/com/gichungasoftwares/book_network/history/BookTransactionHistory.java
@@ -0,0 +1,2 @@
+package com.gichungasoftwares.book_network.history;public class BookTransactionHistory {
+}
